e8477d3f0a298921257c28ba85b1b6ab20666f43
DevAhmedHelmy <helmyahmed248@gmail.com>
2019-09-12 17:16:38 +0200

    install vue and vuetify

---
+++ resources/assets/components/AppHome.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-toolbar>
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <div class="flex-grow-1"></div>
+
+      <v-toolbar-items>
+        <v-btn text>Link 1</v-btn>
+        <v-btn text>Link 2</v-btn>
+        <v-btn text>Link 3</v-btn>
+      </v-toolbar-items>
+
+       
+    </v-toolbar>
+  </div>
+</template>
+<script>
+export default {
+    
+}
+</script>       
+<style lang="scss">
+    
+</style>
---
+++ resources/assets/js/app.js
@@ -0,0 +1,8 @@
+require('./bootstrap');
+ 
+import Vue from 'vue'
+import vuetify from '../plugins/vuetify' // path to vuetify export
+
+new Vue({
+  vuetify,
+}).$mount('#app')
