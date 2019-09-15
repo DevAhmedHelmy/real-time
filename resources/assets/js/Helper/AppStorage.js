ca2d10c987b7b7b0e9249399caab9264bf3ca3c9
DevAhmedHelmy <helmyahmed248@gmail.com>
2019-09-15 18:12:59 +0200

    jjj

--- resources/assets/js/Helper/AppStorage.js
+++ resources/assets/js/Helper/AppStorage.js
@@ -1,5 +1,39 @@
-class Storage{
-    storeToken(token){
-        return localStorage.setItem(token)
+class AppStorage{
+
+    // function to store token
+    storeToken(token)
+    {
+        localStorage.setItem('token' ,token)
+    }
+
+    // function to store user
+    storeUser(user)
+    {
+        localStorage.setItem('user', user);
+    }
+
+    store(user,token)
+    {
+        this.storeToken(token);
+        this.storeUser(user);
+    }
+
+    // when logout will remove data
+    clear()
+    {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+    }
+
+    getToken()
+    {
+        return localStorage.getItem('token');
+    }
+
+    getUser()
+    {
+        return localStorage.getItem('user');
     }
 }
+
+export default AppStorage = new AppStorage();
